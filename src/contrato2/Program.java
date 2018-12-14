843220b6efb5a061477221d4682706f6a7bc08e9
Rodrigo <rodrigogat5@hotmail.com>
2018-12-14 12:36:15 -0200

    implement

---
+++ src/contrato2/Program.java
@@ -0,0 +1,20 @@
+package contrato2;
+
+import java.util.Scanner;
+
+
+
+public class Program {
+
+	public static void main(String[] args) {
+        
+            Scanner sc = new Scanner(System.in);
+            
+            System.out.println("Digite o departamento");
+            String dpName = sc.nextLine();   
+            System.out.println("Digite os dados do funcionario");
+            String nome = sc.nextLine();
+            
+        }
+}
+		
